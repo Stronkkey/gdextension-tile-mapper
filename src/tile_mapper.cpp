8269faef2ee98ff7c90714962594b6ec682e12af
Stronkkey <Ail45000@proton.me>
2023-11-27 17:59:34 +0100

    stuff

--- src/tile_mapper.cpp
+++ src/tile_mapper.cpp
@@ -48,31 +48,31 @@ TileMapper::~TileMapper() {
   clear_cells();
 }
 
-Ref<TileSetAtlasSource> TileMapper::get_atlas_source(const int32_t source_id) const {
+Ref<TileSetAtlasSource> TileMapper::_get_atlas_source(const int32_t source_id) const {
   if (tile_set.is_null() || !tile_set->has_source(source_id))
     return Ref<TileSetAtlasSource>();
   return Ref<TileSetAtlasSource>(tile_set->get_source(source_id));
 }
 
 
-Ref<Texture2D> TileMapper::get_texture_from_source_id(const int32_t source_id) const {
+Ref<Texture2D> TileMapper::_get_texture_from_source_id(const int32_t source_id) const {
   if (tile_set.is_null() || !tile_set->has_source(source_id))
     return Ref<Texture2D>();
-  Ref<TileSetAtlasSource> source = get_atlas_source(source_id);
+  Ref<TileSetAtlasSource> source = _get_atlas_source(source_id);
   return source.is_valid() ? source->get_texture() : Ref<Texture2D>();
 }
 
-Rect2i TileMapper::get_texture_region_from_atlas_source(const int32_t source_id, const Vector2i &atlas_coords) const {
-  Ref<TileSetAtlasSource> source = get_atlas_source(source_id);
+Rect2i TileMapper::_get_texture_region_from_atlas_source(const int32_t source_id, const Vector2i &atlas_coords) const {
+  Ref<TileSetAtlasSource> source = _get_atlas_source(source_id);
   return source.is_valid() ? source->get_tile_texture_region(atlas_coords) : Rect2i();
 }
 
-Rect2i TileMapper::get_texture_region_from_cell_data(CellData *cell_data) const {
-  return get_texture_region_from_atlas_source(cell_data->tile_info.source_id, Vector2i(cell_data->tile_info.x, cell_data->tile_info.y));
+Rect2i TileMapper::_get_texture_region_from_cell_data(CellData *cell_data) const {
+return _get_texture_region_from_atlas_source(cell_data->tile_info.source_id, Vector2i(cell_data->tile_info.x, cell_data->tile_info.y));
 }
 
 
-RID TileMapper::create_shape_for_cell_layer_polygon_index(CellData *cell_data, int32_t layer, int32_t polygon_index) const {
+RID TileMapper::_create_shape_for_cell_layer_polygon_index(CellData *cell_data, int32_t layer, int32_t polygon_index) const {
   PackedVector2Array points = cell_data->tile_data->get_collision_polygon_points(layer, polygon_index);
   if (points.size() <= 2)
     return RID();
@@ -82,12 +82,12 @@ RID TileMapper::create_shape_for_cell_layer_polygon_index(CellData *cell_data, i
   return shape;
 }
 
-RID TileMapper::create_cell_body_for_layer(CellData *cell_data, int32_t layer) const {
+RID TileMapper::_create_cell_body_for_layer(CellData *cell_data, int32_t layer) const {
   std::vector<RID> shapes = {};
   std::vector<ShapeData> shape_datas = {};
 
   for (int32_t polygon_index = 0; polygon_index < cell_data->tile_data->get_collision_polygons_count(layer); polygon_index++) {
-    RID shape = create_shape_for_cell_layer_polygon_index(cell_data, layer, polygon_index);
+    RID shape = _create_shape_for_cell_layer_polygon_index(cell_data, layer, polygon_index);
     if (shape == RID())
       continue;
 
@@ -104,7 +104,7 @@ RID TileMapper::create_cell_body_for_layer(CellData *cell_data, int32_t layer) c
 
   Ref<PhysicsMaterial> material = tile_set->get_physics_layer_physics_material(layer);
   RID body = PhysicsServer2D::get_singleton()->body_create();
-  Size2i cell_size = get_texture_region_from_cell_data(cell_data).get_size();
+  Size2i cell_size = _get_texture_region_from_cell_data(cell_data).get_size();
   Ref<World2D> world_2d = get_world_2d();
   PhysicsServer2D *physics_server2d = PhysicsServer2D::get_singleton();
 
@@ -127,15 +127,15 @@ RID TileMapper::create_cell_body_for_layer(CellData *cell_data, int32_t layer) c
     physics_server2d->body_set_param(body, PhysicsServer2D::BODY_PARAM_BOUNCE, material->get_bounce());
     physics_server2d->body_set_param(body, PhysicsServer2D::BODY_PARAM_FRICTION, material->get_friction());
   }
-
+  UtilityFunctions::print("Body");
   return body;
 }
 
-TypedArray<RID> TileMapper::create_physics_bodies_for_cell(CellData *cell_data) const {
+TypedArray<RID> TileMapper::_create_physics_bodies_for_cell(CellData *cell_data) const {
   TypedArray<RID> bodies = {};
 
   for (int32_t layer = 0; layer < tile_set->get_physics_layers_count(); layer++) {
-    RID body = create_cell_body_for_layer(cell_data, layer);
+    RID body = _create_cell_body_for_layer(cell_data, layer);
     if (body != RID())
       bodies.append(body);
   }
@@ -143,15 +143,16 @@ TypedArray<RID> TileMapper::create_physics_bodies_for_cell(CellData *cell_data) 
   return bodies;
 }
 
-void TileMapper::draw_quadrant(Quadrant *quadrant) {
+void TileMapper::_draw_quadrant(Quadrant *quadrant) {
+  const bool draw_debug_shapes = _should_draw_debug_shapes();
   RenderingServer::get_singleton()->canvas_item_clear(quadrant->canvas_item);
   for (std::pair<int64_t, CellData*> iterator: quadrant->cells) {
-    draw_quadrant_cell(iterator.second, quadrant);
+    _draw_quadrant_cell(iterator.second, quadrant);
   }
 }
 
-void TileMapper::draw_quadrant_cell(CellData *cell_data, Quadrant *quadrant) {
-  Rect2i size_rect = get_texture_region_from_cell_data(cell_data);
+void TileMapper::_draw_quadrant_cell(CellData *cell_data, Quadrant *quadrant) {
+  Rect2i size_rect = _get_texture_region_from_cell_data(cell_data);
   Rect2i texture_rect = Rect2i((size_rect.get_position() + cell_data->tile_data->get_texture_origin() - size_rect.size), size_rect.size);
 
   RenderingServer *rendering_server = RenderingServer::get_singleton();
@@ -162,10 +163,13 @@ void TileMapper::draw_quadrant_cell(CellData *cell_data, Quadrant *quadrant) {
       size_rect,
       cell_data->tile_data->get_modulate(),
       cell_data->tile_data->get_transpose());
+
+  if (_should_draw_debug_shapes())
+    _cell_draw_debug_shape(cell_data, shape_color);
 }
 
 
-Quadrant *TileMapper::create_new_quadrant() const {
+Quadrant *TileMapper::_create_new_quadrant() const {
   Quadrant *new_quadrant = memnew(Quadrant);
   Ref<Material> material = get_material();
   new_quadrant->canvas_item = RenderingServer::get_singleton()->canvas_item_create();
@@ -177,29 +181,30 @@ Quadrant *TileMapper::create_new_quadrant() const {
   return new_quadrant;
 }
 
-bool TileMapper::should_draw_debug_shapes() const {
+bool TileMapper::_should_draw_debug_shapes() const {
   bool debugging_collision_hint = is_inside_tree() && get_tree()->is_debugging_collisions_hint();
   return (debugging_collision_hint && collision_visibility == COLLISION_VISIBILITY_DEFAULT) || collision_visibility == COLLISION_VISIBILITY_ALWAYS;
 }
 
-void TileMapper::cell_draw_debug_shape(CellData *cell_data, const Color &shape_color) {
+void TileMapper::_cell_draw_debug_shape(CellData *cell_data, const Color &shape_color) {
   for (int i = 0; i < cell_data->physics_bodies_rid.size(); i++) {
     RID body = cell_data->physics_bodies_rid[i];
     int shape_count = PhysicsServer2D::get_singleton()->body_get_shape_count(body);
+
     for (int shape_index = 0; i < shape_count; i++) {
-      draw_cell_shape(cell_data, body, shape_index, shape_color);
+      _draw_cell_shape(cell_data, body, shape_index, shape_color);
     }
   }
 }
 
-void TileMapper::draw_cell_shape(CellData *cell_data, const RID &body, const int shape_index, const Color &shape_color) {
-  Size2i cell_size = get_texture_region_from_cell_data(cell_data).get_size();
+void TileMapper::_draw_cell_shape(CellData *cell_data, const RID &body, const int shape_index, const Color &shape_color) {
+  Size2i cell_size = _get_texture_region_from_cell_data(cell_data).get_size();
   RID shape = PhysicsServer2D::get_singleton()->body_get_shape(body, shape_index);
   PhysicsServer2D::ShapeType shape_type = PhysicsServer2D::get_singleton()->shape_get_type(shape);
   Transform2D shape_transform = PhysicsServer2D::get_singleton()->body_get_shape_transform(body, shape_index);
 
   ERR_FAIL_COND_MSG(shape_type != PhysicsServer2D::SHAPE_CONVEX_POLYGON, "Wrong shape type for a tile, should be SHAPE_CONVEX_POLYGON.");
-  RID draw_rid = get_draw_rid_from_cell_data(cell_data);
+  RID draw_rid = _get_draw_rid_from_cell_data(cell_data);
   ERR_FAIL_COND(draw_rid == RID());
 
   PackedColorArray colors = {};
@@ -208,8 +213,77 @@ void TileMapper::draw_cell_shape(CellData *cell_data, const RID &body, const int
   RenderingServer::get_singleton()->canvas_item_add_polygon(draw_rid, PhysicsServer2D::get_singleton()->shape_get_data(shape), colors);
 }
 
-RID TileMapper::get_draw_rid_from_cell_data(CellData *cell_data) const {
-  CellDrawState cell_draw_state = get_cell_draw_state(cell_data);
+void TileMapper::_update_canvas_item_cell(CellData *cell_data) {
+  RenderingServer *rendering_server = RenderingServer::get_singleton();
+  rendering_server->canvas_item_set_transform(cell_data->canvas_rid, cell_data->transform);
+  rendering_server->canvas_item_set_z_index(cell_data->canvas_rid, cell_data->tile_data->get_z_index());
+  rendering_server->canvas_item_set_default_texture_filter(cell_data->canvas_rid, static_cast<RenderingServer::CanvasItemTextureFilter>(get_texture_filter()));
+  rendering_server->canvas_item_set_default_texture_repeat(cell_data->canvas_rid, static_cast<RenderingServer::CanvasItemTextureRepeat>(get_texture_repeat()));
+  rendering_server->canvas_item_set_light_mask(cell_data->canvas_rid, get_light_mask());
+}
+
+void TileMapper::_draw_tile(CellData *cell_data) {
+  RenderingServer *rendering_server = RenderingServer::get_singleton();
+  Rect2i size_rect = _get_texture_region_from_cell_data(cell_data);
+  Rect2i texture_rect = Rect2i((size_rect.position + cell_data->tile_data->get_texture_origin()), size_rect.get_size());
+
+  rendering_server->canvas_item_clear(cell_data->canvas_rid);
+  rendering_server->canvas_item_add_texture_rect_region(cell_data->canvas_rid, 
+      texture_rect,
+      cell_data->texture->get_rid(),
+      size_rect);
+  rendering_server->canvas_item_set_parent(cell_data->canvas_rid, get_canvas_item());
+
+  Ref<Material> material = cell_data->tile_data->get_material();
+  
+  if (material.is_valid())
+    rendering_server->canvas_item_set_material(cell_data->canvas_rid, material->get_rid());
+
+  if (_should_draw_debug_shapes())
+    _cell_draw_debug_shape(cell_data, shape_color);
+}
+
+void TileMapper::_set_cell_to_use_canvas_item_cell(CellData *cell_data) {
+  cell_data->canvas_rid = RenderingServer::get_singleton()->canvas_item_create();
+  if (cell_data->current_quadrant) {
+    auto iterator = cell_data->current_quadrant->cells.find(cell_data->cell_id);
+    if (iterator != cell_data->current_quadrant->cells.end())
+      cell_data->current_quadrant->cells.erase(iterator);
+  }
+  _draw_quadrant(cell_data->current_quadrant);
+  cell_data->current_quadrant = nullptr;
+  _draw_tile(cell_data);
+  _update_canvas_item_cell(cell_data);
+}
+
+void TileMapper::_set_cell_transform(CellData *cell_data, const Transform2D &new_transform) {
+  cell_data->transform = new_transform;
+
+  switch (_get_cell_draw_state(cell_data)) {
+    CANVAS_ITEM:
+      RenderingServer::get_singleton()->canvas_item_set_transform(cell_data->canvas_rid, new_transform);
+      break;
+    QUADRANT:
+      _draw_quadrant(cell_data->current_quadrant);
+      break;
+    default:
+      break;
+  }
+
+  for (int64_t i = 0; i < cell_data->physics_bodies_rid.size(); i++)
+    PhysicsServer2D::get_singleton()->body_set_state(cell_data->physics_bodies_rid[i], PhysicsServer2D::BODY_STATE_TRANSFORM, cell_data->transform);
+}
+
+void TileMapper::_set_cell_to_use_quadrant(CellData *cell_data, Quadrant *quadrant) {
+  if (cell_data->canvas_rid != RID())
+    RenderingServer::get_singleton()->free_rid(cell_data->canvas_rid);
+  cell_data->canvas_rid = RID();
+  quadrant->cells.insert({cell_data->cell_id, cell_data});
+  _draw_quadrant_cell(cell_data, quadrant);
+}
+
+RID TileMapper::_get_draw_rid_from_cell_data(CellData *cell_data) const {
+  CellDrawState cell_draw_state = _get_cell_draw_state(cell_data);
   if (cell_draw_state == NONE) {
     ERR_PRINT("Invalid Cell");
     return RID();
@@ -219,24 +293,13 @@ RID TileMapper::get_draw_rid_from_cell_data(CellData *cell_data) const {
   return cell_draw_state == CANVAS_ITEM ? cell_data->canvas_rid : quadrant_rid; 
 }
 
-TileMapper::CellDrawState TileMapper::get_cell_draw_state(CellData *cell_data) const {
+TileMapper::CellDrawState TileMapper::_get_cell_draw_state(CellData *cell_data) const {
   if (cell_data != nullptr && cell_data->canvas_rid != RID())
     return CANVAS_ITEM;
   return cell_data->current_quadrant != nullptr ? QUADRANT : NONE;
 }
 
-bool TileMapper::destroy_quadrant(Quadrant *quadrant) {
-  if (quadrant == nullptr)
-    return false;
-
-  std::unordered_map<int64_t, CellData*> quadrant_cells = quadrant->cells;
-  quadrant->cells.clear();
-
-  for (std::pair<int64_t, CellData*> iterator: quadrant_cells) {
-    iterator.second->current_quadrant = nullptr;
-    destroy_cell(iterator.first);
-  }
-
+void TileMapper::_destroy_quadrant(Quadrant *quadrant) {
   std::vector<Quadrant*> tile_info_quadrants = {};
   auto iterator = quadrants.find(quadrant->tile_info);
   if (iterator != quadrants.end()) {
@@ -254,10 +317,22 @@ bool TileMapper::destroy_quadrant(Quadrant *quadrant) {
   RenderingServer::get_singleton()->canvas_item_clear(quadrant->canvas_item);
   RenderingServer::get_singleton()->free_rid(quadrant->canvas_item);
   memdelete(quadrant);
-  return true;
 }
 
-void TileMapper::remove_cell(CellData *cell_data, const bool remove_quadrant) {
+void TileMapper::_destroy_quadrant_cells(Quadrant *quadrant) {
+  std::unordered_map<int64_t, CellData*> quadrant_cells = quadrant->cells;
+  for (std::pair<int64_t, CellData*> iterator: quadrant_cells) {
+    auto cell_iterator = tiles.find(iterator.first);
+    if (cell_iterator != tiles.end())
+      tiles.erase(cell_iterator);
+
+    _remove_cell(iterator.second, false);
+  }
+
+  quadrant->cells.clear();
+}
+
+void TileMapper::_remove_cell(CellData *cell_data, const bool remove_quadrant) {
   Quadrant *cell_quadrant = cell_data->current_quadrant;
   PhysicsServer2D *physics_server2d = PhysicsServer2D::get_singleton();
   RenderingServer *rendering_server = RenderingServer::get_singleton();
@@ -266,15 +341,11 @@ void TileMapper::remove_cell(CellData *cell_data, const bool remove_quadrant) {
     rendering_server->free_rid(cell_data->canvas_rid);
   }
 
-  /*if (remove_quadrant && cell_quadrant) {
+  if (remove_quadrant && cell_quadrant != nullptr) {
     auto quadrant_cells_iterator = cell_quadrant->cells.find(cell_data->cell_id);
     if (quadrant_cells_iterator != cell_quadrant->cells.end())
       cell_quadrant->cells.erase(cell_data->cell_id);
-    if (cell_quadrant->cells.empty())
-      destroy_quadrant(cell_quadrant);
-    else
-      draw_quadrant(cell_quadrant);
-  }*/
+  }
 
   int64_t physics_bodies_rid_size = cell_data->physics_bodies_rid.size();
 
@@ -292,7 +363,7 @@ void TileMapper::remove_cell(CellData *cell_data, const bool remove_quadrant) {
   memdelete(cell_data);
 }
 
-Quadrant *TileMapper::get_quadrant_with_tile_info(TileInfo tile_info) {
+Quadrant *TileMapper::_get_quadrant_with_tile_info(TileInfo tile_info) {
   std::vector<Quadrant*> cell_quadrants = {};
   auto iterator = quadrants.find(tile_info);
 
@@ -303,8 +374,8 @@ Quadrant *TileMapper::get_quadrant_with_tile_info(TileInfo tile_info) {
   if (!cell_quadrants.empty()) {
     Quadrant *last_quadrant = cell_quadrants.back();
     bool new_quadrant = last_quadrant->cells.size() >= quadrant_size;
-    Quadrant *quadrant = last_quadrant->cells.size() >= quadrant_size ? create_new_quadrant() : last_quadrant;
-    Ref<TileSetAtlasSource> source = get_atlas_source(tile_info.source_id);
+    Quadrant *quadrant = last_quadrant->cells.size() >= quadrant_size ? _create_new_quadrant() : last_quadrant;
+    Ref<TileSetAtlasSource> source = _get_atlas_source(tile_info.source_id);
   
     if (last_quadrant->cells.size() <= quadrant_size && source.is_valid()) {
       quadrant->tile_info = tile_info;
@@ -323,16 +394,16 @@ Quadrant *TileMapper::get_quadrant_with_tile_info(TileInfo tile_info) {
     return quadrant;
   }
 
-  Quadrant *new_quadrant = create_new_quadrant();
+  Quadrant *new_quadrant = _create_new_quadrant();
   std::vector<Quadrant*> new_quadrant_vector = {new_quadrant};
   quadrants.insert({tile_info, new_quadrant_vector});
   return new_quadrant;
 }
 
-void TileMapper::create_new_cell(const Vector2 &coords, const int32_t source_id, const Vector2i &atlas_coords, const int alternative_tile_id) {
+void TileMapper::_create_new_cell(const Vector2 &coords, const int32_t source_id, const Vector2i &atlas_coords, const int alternative_tile_id) {
   ERR_FAIL_COND_MSG(tile_set.is_null(), "Tried adding cell with no TileSet.");
   ERR_FAIL_COND_MSG(!tile_set->has_source(source_id), vformat("TileSet source with id %s does not exist.", source_id));
-  Ref<TileSetAtlasSource> source = get_atlas_source(source_id);
+  Ref<TileSetAtlasSource> source = _get_atlas_source(source_id);
 
   ERR_FAIL_COND_MSG(source.is_null(), vformat("Tile source with id %s is not a TileSetAtlasSource.", source_id));
   ERR_FAIL_COND_MSG(!source->has_tile(atlas_coords), vformat("No tile at %s.", atlas_coords));
@@ -347,22 +418,19 @@ void TileMapper::create_new_cell(const Vector2 &coords, const int32_t source_id,
   tile_info.alternative_tile_id = alternative_tile_id;
 
   cell_data->cell_id = cell_id;
-  cell_data->texture = get_texture_from_source_id(source_id);
+  cell_data->texture = _get_texture_from_source_id(source_id);
   cell_data->tile_info = tile_info;
   cell_data->transform = Transform2D(0, coords);
   cell_data->tile_data = source->get_tile_data(atlas_coords, alternative_tile_id);
-  cell_data->physics_bodies_rid = create_physics_bodies_for_cell(cell_data);
+  cell_data->physics_bodies_rid = _create_physics_bodies_for_cell(cell_data);
 
-  Quadrant *quadrant = get_quadrant_with_tile_info(tile_info);
+  Quadrant *quadrant = _get_quadrant_with_tile_info(tile_info);
 
   quadrant->cells.insert({cell_id, cell_data});
   tiles.insert({cell_id, cell_data});
 
   cell_data->current_quadrant = quadrant;
-  draw_quadrant_cell(cell_data, quadrant);
-
-  if (should_draw_debug_shapes())
-    cell_draw_debug_shape(cell_data, shape_color);
+  _draw_quadrant_cell(cell_data, quadrant);
 }
 
 
@@ -371,7 +439,7 @@ int64_t TileMapper::add_cell(const Vector2 &coords, const int32_t source_id, con
     index++;
 
   int64_t cell_id = index;
-  create_new_cell(coords, source_id, atlas_coords, alternative_tile_id);
+  _create_new_cell(coords, source_id, atlas_coords, alternative_tile_id);
   
   index++;
   return cell_id;
@@ -380,7 +448,13 @@ int64_t TileMapper::add_cell(const Vector2 &coords, const int32_t source_id, con
 bool TileMapper::destroy_cell(const int64_t cell_id) {
   auto iterator = tiles.find(cell_id);
   if (iterator != tiles.end()) {
-    remove_cell(iterator->second);
+    CellData *cell_data = iterator->second;
+    Quadrant *quadrant = cell_data->current_quadrant;
+
+    if (quadrant != nullptr && quadrant->cells.size() == 1)
+      _destroy_quadrant(quadrant);
+
+    _remove_cell(cell_data);
     return true;
   }
 
@@ -393,16 +467,16 @@ void TileMapper::clear_cells() {
   
   for (auto iterator: local_quadrants) {
     std::vector<Quadrant*> local_local_quadrants = iterator.second;
-    for (Quadrant *quadrant: local_local_quadrants)
-      destroy_quadrant(quadrant);
-  }
-
-  for (auto iterator: tiles) {
-    remove_cell(iterator.second, false);
+    for (Quadrant *quadrant: local_local_quadrants) {
+      _destroy_quadrant_cells(quadrant);
+      _destroy_quadrant(quadrant);
+    }
   }
 
   tiles.clear();
   quadrants.clear();
+  UtilityFunctions::print(tiles.size());
+  UtilityFunctions::print(quadrants.size());
 }
 
 bool TileMapper::is_cell_id_valid(const int64_t cell_id) const {
--- src/tile_mapper.hpp
+++ src/tile_mapper.hpp
@@ -33,7 +33,7 @@ private:
     COLLISION_VISIBILITY_NONE = 2,
   };
 
-  const Color shape_color = ProjectSettings::get_singleton()->get("debug/shapes/collision/shape_color");
+  const Color shape_color = ProjectSettings::get_singleton()->get_setting("debug/shapes/collision/shape_color", Color());
 
   Ref<TileSet> tile_set;
   PhysicsServer2D::BodyMode collision_type;
@@ -45,29 +45,35 @@ private:
   std::unordered_map<TileInfo, std::vector<Quadrant*>> quadrants;
   std::unordered_map<int64_t, CellData*> tiles;
 
-  Ref<TileSetAtlasSource> get_atlas_source(const int32_t source_id) const;
-  Ref<Texture2D> get_texture_from_source_id(const int32_t source_id) const;
-  Rect2i get_texture_region_from_atlas_source(const int32_t source_id, const Vector2i &atlas_coords) const;
-  Rect2i get_texture_region_from_cell_data(CellData *cell_data) const;
+  Ref<TileSetAtlasSource> _get_atlas_source(const int32_t source_id) const;
+  Ref<Texture2D> _get_texture_from_source_id(const int32_t source_id) const;
+  Rect2i _get_texture_region_from_atlas_source(const int32_t source_id, const Vector2i &atlas_coords) const;
+  Rect2i _get_texture_region_from_cell_data(CellData *cell_data) const;
   
-  RID create_shape_for_cell_layer_polygon_index(CellData *cell_data, const int32_t layer, const int32_t polygon_index) const;
-  RID create_cell_body_for_layer(CellData *cell_data, const int32_t layer) const;
-  TypedArray<RID> create_physics_bodies_for_cell(CellData *cell_data) const;
+  RID _create_shape_for_cell_layer_polygon_index(CellData *cell_data, const int32_t layer, const int32_t polygon_index) const;
+  RID _create_cell_body_for_layer(CellData *cell_data, const int32_t layer) const;
+  TypedArray<RID> _create_physics_bodies_for_cell(CellData *cell_data) const;
 
-  void draw_quadrant(Quadrant *quadrant);
-  void draw_quadrant_cell(CellData *cell_data, Quadrant *quadrant);
-  bool should_draw_debug_shapes() const;
-  void cell_draw_debug_shape(CellData *cell_data, const Color &shape_color);
-  void draw_cell_shape(CellData *cell_data, const RID &body, const int shape_index, const Color &shape_color);
+  void _draw_quadrant(Quadrant *quadrant);
+  void _draw_quadrant_cell(CellData *cell_data, Quadrant *quadrant);
+  bool _should_draw_debug_shapes() const;
+  void _cell_draw_debug_shape(CellData *cell_data, const Color &shape_color);
+  void _draw_cell_shape(CellData *cell_data, const RID &body, const int shape_index, const Color &shape_color);
+  void _update_canvas_item_cell(CellData *cell_data);
+  void _draw_tile(CellData *cell_data);
+  void _set_cell_to_use_canvas_item_cell(CellData *cell_data);
+  void _set_cell_to_use_quadrant(CellData *cell_data, Quadrant *quadrant);
+  void _set_cell_transform(CellData *cell_data, const Transform2D &new_transform);
 
-  RID get_draw_rid_from_cell_data(CellData *cell_data) const;
-  CellDrawState get_cell_draw_state(CellData *cell_data) const;
-  bool destroy_quadrant(Quadrant *quadrant);
-  void remove_cell(CellData *cell_data, const bool remove_quadrant = true);
+  RID _get_draw_rid_from_cell_data(CellData *cell_data) const;
+  CellDrawState _get_cell_draw_state(CellData *cell_data) const;
+  void _destroy_quadrant(Quadrant *quadrant);
+  void _destroy_quadrant_cells(Quadrant *quadrant);
+  void _remove_cell(CellData *cell_data, const bool remove_quadrant = true);
 
-  Quadrant *create_new_quadrant() const;
-  Quadrant *get_quadrant_with_tile_info(TileInfo tile_info);
-  void create_new_cell(const Vector2 &coords, const int32_t source_id, const Vector2i &atlas_coords = Vector2i(), const int alternative_tile_id = 0);
+  Quadrant *_create_new_quadrant() const;
+  Quadrant *_get_quadrant_with_tile_info(TileInfo tile_info);
+  void _create_new_cell(const Vector2 &coords, const int32_t source_id, const Vector2i &atlas_coords = Vector2i(), const int alternative_tile_id = 0);
 
 protected:
   static void _bind_methods();
